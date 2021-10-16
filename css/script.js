941d91e21d39320e4628d10c75d6a0a0d3245301
RufusCarlito <66885342+RufusCarlito@users.noreply.github.com>
2021-10-16 02:37:20 +0200

    v1.3

---
+++ css/script.js
@@ -0,0 +1,10 @@
+// Funkcje
+
+var result = document.getElementById("result");
+
+function hello()
+{
+    alert("Witaj! Znalazłeś się na stronie, która jest moją wizytówką, miłej lektury.");
+}
+
+hello();
